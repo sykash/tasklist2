6729c0aa2d3f45a52bfd8d150c4b4747e9a6839e
hssyk <hssyk@LAPTOP-R1K5Q1S7.lan>
2023-03-19 20:50:37 +0900

    「データベース関連のファイルを追加」

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,3 +1,19 @@
+public class DBUtil {
+    private static final String PERSISTENCE_UNIT_NAME = "message_board";
+    private static EntityManagerFactory emf;
+
+    public static EntityManager createEntityManager() {
+        return getEntityManagerFactory().createEntityManager();
+    }
+
+    private static EntityManagerFactory getEntityManagerFactory() {
+        if(emf == null) {
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        }
+
+        return emf;
+    }
+}
 package controllers;
 
 import java.io.IOException;
